c1c47526f3b47b62cf0e85f5fc1138da3b0f81d7
Gabriel Morais <bielmorais434@gmail.com>
2026-06-01 22:52:15 -0300

    Feat: criando comando Init

--- commands/init.py
+++ commands/init.py
@@ -1,4 +1,7 @@
 from pathlib import Path
+import os
+
+os.system('color')
 
 # Tabela de cores ANSI
 RESET = "\033[0m"
@@ -15,8 +18,20 @@ class Init:
     def run(self):
         diretorio = Path('.')
         if any(diretorio.glob('*.meugit')):
-            print(f"{VERMELHO}Project already initialized.{RESET}")
+            print(f"{VERMELHO}Projeto .meugit já inicializado.{RESET}")
             return
         else:
             print(f"{VERDE}Inicializando o projeto...{RESET}")
+            dir_meugit = Path(".meugit")
+            dir_meugit_commits = Path(".meugit/commits")
+            dir_meugit_objects = Path(".meugit/objects")
+            dir_meugit_branches = Path(".meugit/branches")
+            dir_meugit_head = Path(".meugit/HEAD")
+
+
+            dir_meugit.mkdir(exist_ok=True)
+            dir_meugit_commits.mkdir(exist_ok=True)
+            dir_meugit_objects.mkdir(exist_ok=True)
+            dir_meugit_branches.mkdir(exist_ok=True)
+            dir_meugit_head.mkdir(exist_ok=True)
         
